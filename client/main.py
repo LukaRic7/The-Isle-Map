d6ba5ca036ef9c65f7c2b1f76beda5e84cfc8c0a
LukaRic <lukajacobsen1712@gmail.com>
2026-02-01 12:32:13 +0100

    Added comments and docstrings

--- client/main.py
+++ client/main.py
@@ -1,34 +1,61 @@
+import socketio, os, json
 import loggerric as lr
 import tkinter as tk
-import sockric as sr
-import os, json
 
 from gui import Gui
+import utils
 
-with open(os.path.join(os.path.dirname(__file__), 'config.json')) as file:
+# Initialize client
+sio = socketio.Client()
+
+# Read the config
+with open(utils.resource_path('config.json')) as file:
     CONFIG:dict = json.load(file)
 
-server_config:dict = CONFIG.get('server', {})
-client = sr.Client(server_config.get('ip'), server_config.get('port'))
-
+# Create a tkinter root
 root = tk.Tk()
 root.wm_title('The Isle Map v3.0')
 root.wm_resizable(False, False)
 
-app = Gui(root, client, CONFIG)
+# Create an instance of the GUI app
+app = Gui(root, sio, CONFIG)
 app.pack()
 
-@client.on_packet(sr.EventDefaults.SERVER_STOPPED)
-def on_server_stopped(data:dict) -> None:
-    print(data, 'server_stopped')
+# Called when client connected successfully
+@sio.event
+def connect():
+    """Called when the client connected successfully. Tell the server aswell."""
 
-@client.on_packet('update_positions')
-def on_update_positions(data:dict) -> None:
-    print(data, 'update_positions')
+    lr.Log.info('Connected!')
+    sio.emit('client-connect')
 
-@client.on_packet('color_assignment')
-def on_color_assignment(data:dict) -> None:
-    print(data, 'color_assignment')
+# Called when any client on the server updates their position
+@sio.on("update-position")
+def on_update(players_positions:str):
+    """
+    Called when any client on the server updates their position. Then updates
+    this clients positions on the map.
+    
+    *Parameters*:
+    - `players_positions` (str): Serialized positions of all players. Can be
+    deserialized to a dictionary with key being the clients color, and value
+    being the list of positions.
+    """
+
+    app.update_positions_online(players_positions)
+
+# Called when the server assigns this client a color
+@sio.on('color-assignment')
+def on_color_assignment(color:str):
+    """
+    Called when the server assigns this client a color. Then updates the color
+    in the GUI on this client.
+    
+    *Parameters*:
+    - `color` (str): The color the server assigned this client.
+    """
+
+    app.set_color(color)
 
 if __name__ == '__main__':
     root.mainloop()
